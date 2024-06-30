ac63422497162c3e8357812eb12fdfca3e3d57c4
LAMSSAOUI ABDELALI <lam998216@gmail.com>
2024-06-30 19:04:53 +0200

    the update function work correctly now

--- pages/settings/index.js
+++ pages/settings/index.js
@@ -61,6 +61,33 @@ const Index = () => {
       }
     };
 
+    const handleUpdateUser = async () => {
+      const response = await fetch('/api/users', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id,
+          username,
+          password: newPassword,
+          role: userData ? userData.role : 'user',
+        }),
+      });
+  
+      if (response.ok) {
+        alert('User updated successfully');
+        resetForm()
+      } else {
+        const errorData = await response.json();
+        alert(`Error updating user: ${errorData.message}`);
+      }
+    };
+    const resetForm = () => {
+      setNewPassword('')
+      setCurrentPassword('')
+  };
+
   return (
     <div>
       <header className='sticky top-0 z-50'><Navbar user_id={id} /></header>
@@ -131,8 +158,7 @@ const Index = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
               </svg>
             </div>
-            <p className="mt-2">Can't remember your current password? <a className="text-sm font-semibold text-blue-600 underline decoration-2" href="#">Recover Account</a></p>
-            <button onClick={handleSavePassword} className="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white">Save Password</button>
+            <button onClick={handleUpdateUser} className="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white mt-5">Update Password</button>
             <hr className="mt-4 mb-8" />
           </div>
         </div>
