800362398ee594f584551eec008a4f1bce88b322
LAMSSAOUI ABDELALI <lam998216@gmail.com>
2024-06-30 19:05:58 +0200

    update the margin of the buttons

--- pages/settings/index.js
+++ pages/settings/index.js
@@ -158,7 +158,7 @@ const Index = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
               </svg>
             </div>
-            <button onClick={handleUpdateUser} className="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white mt-5">Update Password</button>
+            <button onClick={handleUpdateUser} className=" rounded-lg bg-blue-600 px-4 py-2 text-white mt-5">Update Password</button>
             <hr className="mt-4 mb-8" />
           </div>
         </div>
