b18afb2a855f09b623d7df135a1374f485eb46bb
LAMSSAOUI ABDELALI <lam998216@gmail.com>
2024-07-13 15:52:32 +0200

    adding info enpoint

---
+++ pages/api/info/index.js
@@ -0,0 +1,32 @@
+import { executeQuery } from "../../../config/db";
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const { userId } = req.query;
+
+    if (!userId) {
+      return res.status(400).json({ message: 'User ID is required' });
+    }
+
+    try {
+      const sapQuery = `SELECT COUNT(*) as sapDemandCount FROM sap WHERE user_id = ?`;
+      const sageQuery = `SELECT COUNT(*) as sageDemandCount FROM sage WHERE user_id = ?`;
+
+      const [sapResults] = await executeQuery(sapQuery, [userId]);
+      const [sageResults] = await executeQuery(sageQuery, [userId]);
+
+      const totalDemandCount = sapResults[0].sapDemandCount + sageResults[0].sageDemandCount;
+
+      return res.status(200).json({
+        sapDemandCount: sapResults[0].sapDemandCount,
+        sageDemandCount: sageResults[0].sageDemandCount,
+        totalDemandCount: totalDemandCount
+      });
+    } catch (error) {
+      console.error('Error fetching demands count:', error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  } else {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+}
